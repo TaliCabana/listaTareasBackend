17b106bd2d3f41eedac6f3b4764af99695ff5fcf
Tali <talicabana@gmail.com>
2025-10-07 18:57:24 -0300

    Corregimos la ruta del archivo estático y agregamos los comandos start y dev en el package.json

--- src/server/config.js
+++ src/server/config.js
@@ -1,29 +1,34 @@
-import express from 'express';
-import cors from 'cors';
-import morgan from 'morgan';
-import {dirname} from 'path'
-import {fileURLToPath} from 'url'
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
 // 1- Toma un puerto
 // 2- Configura los middlewares
 // 3- Usa las rutas
 
 export default class Server {
-    constructor() {
-        this.app = express ()
-        this.port = process.env.PORT || 3001
-        this.middlewares()
-    }
-middlewares() {
-    this.app.use(cors()) // permite conexiones remotas
-    this.app.use(morgan('dev')) // permite interpretar los datos que llegan en la solicitud
-    this.app.use(express.json()) // permite interpretar datos que lleguen en la solicitud en formato json
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT || 3001;
+    this.middlewares();
+  }
+  middlewares() {
+    this.app.use(cors()); // permite conexiones remotas
+    this.app.use(morgan("dev")); // permite interpretar los datos que llegan en la solicitud
+    this.app.use(express.json()); // permite interpretar datos que lleguen en la solicitud en formato json
     // Configurar el archivo estatico (carpeta publica)
-    const __dirname = dirname(fileURLToPath(import.meta.url))
-    console.log(__dirname + '/public')
-    this.app.use(express.static(__dirname + '/public'))
-}
-    listen() {
-        this.app.listen(this.port, () => console.log('El servidor está ejecutando en el puerto:' + this.port))
-    }
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    console.log(__dirname);
+    console.log(__dirname + "../../public");
+    this.app.use(express.static(__dirname + "/../../public"));
+  }
+  listen() {
+    this.app.listen(this.port, () =>
+      console.log(
+        `El servidor está ejecutando en: http://localhost:${this.port}`
+      )
+    );
+  }
 }
