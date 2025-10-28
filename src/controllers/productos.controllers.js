58be65cfb96171489b9fe7da9cb43aeda00fa0e4
Tali <talicabana@gmail.com>
2025-10-27 23:47:46 -0300

    Terminé de reorganizar el controlador

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -31,43 +31,56 @@ export const listarProductos = async (req, res) => {
 
 export const obtenerProducto = async (req, res) => {
   try {
-    console.log(req.param.id)
+    console.log(req.param.id);
     const productoBuscado = await Producto.findById(req.params.id);
-    if(!productoBuscado){
-      return res.status(404).json({mensaje: "No se encontó la tarea buscada"})
+    if (!productoBuscado) {
+      return res
+        .status(404)
+        .json({ mensaje: "No se encontó la tarea buscada" });
     }
-  } catch (error){
+  } catch (error) {
     console.error(error);
-    res.status(500).json({mensaje: "Ocurrió un error al obtener el producto"})
-  } res.status(200).json(productoBuscado)
-}
+    res
+      .status(500)
+      .json({ mensaje: "Ocurrió un error al obtener el producto" });
+  }
+  res.status(200).json(productoBuscado);
+};
 
 export const borrarProductoPorId = async (req, res) => {
-  try{
-    console.log(req.params.id);
-    const productoBuscado =await Producto.findById(req.params.id);
-    if(!productoBuscado){
-      return res.status(404).json({mensaje: "No se encontró el producto buscado"})
+  try {
+    const productoBuscado = await Producto.findByIdAndDelete(req.params.id);
+    console.log(productoBuscado);
+    if (!productoBuscado) {
+      return res
+        .status(404)
+        .json({ mensaje: "No se encontró la tarea buscada" });
     }
-    await Producto.findByIdAndDelete (req.params.id)
-    res.status(200).json({mensaje: "La tarea fue eliminada correctamente"})
-  } catch (error){
-    console.error(error)
-    res.status(500).json({mensaje: "Ocurrió un error al eliminar la tarea"})
+    res.status(200).json({ mensaje: "La tarea fue eliminada correctamente" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensaje: "Ocurrió un error al eliminar la tarea" });
   }
-}
+};
 
 export const editarProductoPorId = async (req, res) => {
   try {
-    const productoBuscado =await Producto.findById(req.params.id);
-    if(!productoBuscado){
-      return res.status(4040).json({mensaje: "No se encontró la tarea buscada"})
-    } await Producto.findByIdAndUpdate(req.params.id.req.body)
-    res.status(200).json({mensaje: "La tarea fue editada correctamente"})
+    const productoBuscado = await Producto.findByIdAndUpdate(
+      req.params.id,
+      req.body
+    );
+    if (!productoBuscado) {
+      return res
+        .status(404)
+        .json({ mensaje: "No se encontró la tarea buscada" });
+    }
+    res.status(200).json({ mensaje: "La tarea fue editada correctamente" });
   } catch (error) {
     console.error(error);
-    res.status(500).json({mensaje: "Ocurrió un error, no se pudo editar el producto"})
+    res
+      .status(500)
+      .json({ mensaje: "Ocurrió un error, no se pudo editar el producto" });
   }
-}
+};
 
 export default prueba;
